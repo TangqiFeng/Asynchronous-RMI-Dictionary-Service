314aca853f85e1aa150371398f4a5a690315846c
KyleFeng <kylezete@gmail.com>
2017-12-17 19:27:25 +0000

    initial tomcat servlet

---
+++ RMI_Dictionary_Service/src/ie/gmit/sw/ServiceHandler.java
@@ -0,0 +1,15 @@
+package ie.gmit.sw;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ServiceHandler extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
